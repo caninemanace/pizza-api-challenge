d9197318453a277104510f9ede0d2cd9199fdca2
Eddie Njoroge <eddie.njoroge2306@gmail.com>
2025-06-16 09:59:17 +0300

    created the tables for all models

---
+++ server/models/pizza.py
@@ -0,0 +1,14 @@
+from server.models import db
+from sqlalchemy.orm import relationship
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    ingredients = db.Column(db.String, nullable=False)
+
+    restaurant_pizzas = relationship("RestaurantPizza", back_populates="pizza")
+
+    def __repr__(self):
+        return f"<Pizza {self.name}>"
---
+++ server/models/restaurant.py
@@ -0,0 +1,14 @@
+from server.models import db
+from sqlalchemy.orm import relationship
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+
+    restaurant_pizzas = relationship("RestaurantPizza", back_populates="restaurant", cascade="all, delete")
+
+    def __repr__(self):
+        return f"<Restaurant {self.name}>"
