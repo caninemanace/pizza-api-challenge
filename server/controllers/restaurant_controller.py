3b1d2a90fac24f5bdfe6a44f10103e4704ca2459
Eddie Njoroge <eddie.njoroge2306@gmail.com>
2025-06-16 12:15:02 +0300

    delete end point for the restaurants added

--- server/controllers/restaurant_controller.py
+++ server/controllers/restaurant_controller.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify
 from server.models.restaurant import Restaurant
+from server.models import db
+from server.models.restaurant_pizza import RestaurantPizza
 
 restaurant_bp = Blueprint("restaurant_bp", __name__, url_prefix="/restaurants")
 
@@ -8,4 +10,19 @@ def get_restaurants():
     restaurants = Restaurant.query.all()
     response = [restaurant.to_dict() for restaurant in restaurants]
     return jsonify(response), 200
+
+@restaurant_bp.route("/<int:id>", methods=["DELETE"])
+def delete_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+
+    if not restaurant:
+        return jsonify({"error": "Restaurant not found"}), 404
+
+    # Delete associated RestaurantPizzas first (if cascade not set)
+    RestaurantPizza.query.filter_by(restaurant_id=id).delete()
+
+    db.session.delete(restaurant)
+    db.session.commit()
+
+    return '', 204
 
